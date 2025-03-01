1f5b5c9c6c01d4d5da93b0853a7fca63583c5226
alejones <61201699+alejones@users.noreply.github.com>
2025-02-28 16:36:38 -0800

    actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alejones.github.io',
+	site: 'https://alexjones.sh',
   	// base: 'my-repo',
 	integrations: [mdx(), sitemap()],
 });
